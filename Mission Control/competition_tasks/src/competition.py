5eca49d903433a201b666ede7dd329d3fa29549e
EdCarrasco <carrasco.edw@gmail.com>
2019-11-01 19:08:02 -0700

    added competition.py

---
+++ Mission Control/competition_tasks/src/competition.py
@@ -0,0 +1,11 @@
+import rospy
+import smach
+import smach_ros
+from std_msgs.msg import Int16, Float64, Bool
+import seadragon_states as state
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
